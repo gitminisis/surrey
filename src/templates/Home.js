594fb828fe9b4d618ce679d9cd77706e7588cea4
opacteam <mike@minisisinc.com>
2023-04-04 01:19:27 -0700

    save current dev

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -12,93 +12,81 @@ import Category3 from "../assets/images/Audio-Visual.png";
 import Category4 from "../assets/images/Artifacts.png";
 import Category5 from "../assets/images/Textual Records.png";
 
-const Home = [
-  {
+const Home = [{
     component: "Layout",
     data: {
-      active: "Home",
+        active: "Home",
     },
-    children: [
-      {
-        component: "SimpleSearchBanner",
-        data: {
-          searchURL:
-            "/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM",
-          bannerURL: pic1,
-          heading: "City of Surrey Online Heritage Search",
-          description:
-            "More than 100,000 archival photos, maps, documents, and oral histories, as well as over 5,000 artifacts are at your fingertips. Browse the categories, neighbourhoods, or new additions below, or search for specific records using the keyword search.",
-        },
-      },
-
-      {
-        component: "Section",
-        data: {
-          heading: "Browse by category",
-        },
-        children: [
-          {
-            component: "ImageCarousel",
+    children: [{
+            component: "SimpleSearchBanner",
             data: {
-              loop: true,
-              data: [
-                {
-                  thumbnail: Category1,
-                  title: "Photographs",
-                  link: '/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=DESCRIPTION&language=144&REPORT=WEB_UNION_SUM_DESC&EXP=A_IM_ACCESS present and FORM "Graphic material"',
-                },
-                {
-                  thumbnail: Category2,
-                  title: "Maps",
-                  link: '/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=DESCRIPTION&language=144&REPORT=WEB_UNION_SUM_DESC&EXP=A_IM_ACCESS present and FORM "Cartographic material"',
-                },
-                {
-                  thumbnail: Category3,
-                  title: "Audio/Video",
-                  link: '/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=DESCRIPTION&language=144&REPORT=WEB_UNION_SUM_DESC&EXP=(A_AD_ACCESS present or A_VD_ACCESS present) and FORM "Cartographic material"',
-                },
-                {
-                  thumbnail: Category4,
-                  title: "Artifacts",
-                  link: '/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM&EXP=(A_IM_ACCESS present and FORM "Object") or (M_IM_ACCESS present)',
-                },
-                {
-                  thumbnail: Category5,
-                  title: "Textual Records",
-                  link: '/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=DESCRIPTION&language=144&REPORT=WEB_UNION_SUM_DESC&EXP=A_IM_ACCESS present and FORM "Textual record"',
-                },
-              ],
+                searchURL: "/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM",
+                bannerURL: pic1,
+                heading: "City of Surrey Online Heritage Search",
+                description: "More than 100,000 archival photos, maps, documents, and oral histories, as well as over 5,000 artifacts are at your fingertips. Browse the categories, neighbourhoods, or new additions below, or search for specific records using the keyword search.",
+                bannerCarousel: [pic1, pic2, pic3, pic4, pic5, pic6, pic7],
             },
-          },
-        ],
-      },
-      {
-        component: "Section",
-        data: {
-          heading: "Browse by area",
         },
-        children: [
-          {
-            component: "Map",
-          },
-        ],
-      },
 
-      {
-        component: "Section",
-        data: {
-          heading: "Recent Additions",
-          description:
-            "Recent additions from the Archives and Artifact Collections",
+        {
+            component: "Section",
+            data: {
+                heading: "Browse by category",
+            },
+            children: [{
+                component: "ImageCarousel",
+                data: {
+                    loop: true,
+                    data: [{
+                            thumbnail: Category1,
+                            title: "Photographs",
+                            link: '/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=DESCRIPTION&language=144&REPORT=WEB_UNION_SUM_DESC&EXP=A_IM_ACCESS present and FORM "Graphic material"',
+                        },
+                        {
+                            thumbnail: Category2,
+                            title: "Maps",
+                            link: '/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=DESCRIPTION&language=144&REPORT=WEB_UNION_SUM_DESC&EXP=A_IM_ACCESS present and FORM "Cartographic material"',
+                        },
+                        {
+                            thumbnail: Category3,
+                            title: "Audio/Video",
+                            link: '/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=DESCRIPTION&language=144&REPORT=WEB_UNION_SUM_DESC&EXP=(A_AD_ACCESS present or A_VD_ACCESS present) and FORM "Cartographic material"',
+                        },
+                        {
+                            thumbnail: Category4,
+                            title: "Artifacts",
+                            link: '/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM&EXP=(A_IM_ACCESS present and FORM "Object") or (M_IM_ACCESS present)',
+                        },
+                        {
+                            thumbnail: Category5,
+                            title: "Textual Records",
+                            link: '/scripts/mwimain.dll?UNIONSEARCH&KEEP=Y&SIMPLE_EXP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=DESCRIPTION&language=144&REPORT=WEB_UNION_SUM_DESC&EXP=A_IM_ACCESS present and FORM "Textual record"',
+                        },
+                    ],
+                },
+            }, ],
         },
-        children: [
-          {
-            component: "ImageMasonry",
-            data: {},
-          },
-        ],
-      },
+        {
+            component: "Section",
+            data: {
+                heading: "Browse by area",
+            },
+            children: [{
+                component: "Map",
+            }, ],
+        },
+
+        {
+            component: "Section",
+            data: {
+                heading: "Recent Additions",
+                description: "Recent additions from the Archives and Artifact Collections",
+            },
+            children: [{
+                component: "ImageMasonry",
+                data: {},
+            }, ],
+        },
     ],
-  },
-];
+}, ];
 export default Home;
