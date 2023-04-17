0c21abaa2037185ecbabb54cc74a18580912b088
opacteam <mike@minisisinc.com>
2023-04-16 23:54:23 -0700

    update home exp

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -1,88 +1,99 @@
 import homeSimpleSearchBanner from "./json/homeSimpleSearchBanner.json";
 
-const Home = [{
+const Home = [
+  {
     component: "Layout",
     data: {
-        active: "Home",
+      active: "Home",
     },
-    children: [{
-            component: "SimpleSearchBanner",
-            data: {
-                ...homeSimpleSearchBanner,
-                searchURL: "/scripts/mwimain.dll?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM",
+    children: [
+      {
+        component: "SimpleSearchBanner",
+        data: {
+          ...homeSimpleSearchBanner,
+          searchURL:
+            "/scripts/mwimain.dll?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM",
 
-                bannerCarousel: [
-                    "/assets/images/Union Background 1.png",
-                    "/assets/images/Union Background 2.png",
-                    "/assets/images/Union Background 3.png",
-                    "/assets/images/Union Background 4.png",
-                    "/assets/images/Union Background 5.png",
-                    "/assets/images/Union Background 6.png",
-                    "/assets/images/Union Background 7.png",
-                ],
-            },
+          bannerCarousel: [
+            "/assets/images/Union Background 1.png",
+            "/assets/images/Union Background 2.png",
+            "/assets/images/Union Background 3.png",
+            "/assets/images/Union Background 4.png",
+            "/assets/images/Union Background 5.png",
+            "/assets/images/Union Background 6.png",
+            "/assets/images/Union Background 7.png",
+          ],
         },
+      },
 
-        {
-            component: "Section",
+      {
+        component: "Section",
+        data: {
+          heading: "Browse by category",
+        },
+        children: [
+          {
+            component: "ImageCarousel",
             data: {
-                heading: "Browse by category",
-            },
-            children: [{
-                component: "ImageCarousel",
-                data: {
-                    loop: true,
-                    data: [{
-                            thumbnail: "/assets/images/Photos.png",
-                            title: "Photographs",
-                            link: '?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM&EXP=IMAGE_READY Yes and FORM_CL "Graphic material"',
-
-                        },
-                        {
-                            thumbnail: "/assets/images/Maps.png",
-                            title: "Maps",
-                            link: '?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM&EXP=IMAGE_READY Yes and FORM_CL "Cartographic material"',
-                        },
-                        {
-                            thumbnail: "/assets/images/Audio-Visual.png",
-                            title: "Audio/Video",
-                            link: '?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM&EXP=(AUDIO_READY Yes or VIDEO_READY Yes) and FORM_CL "Cartographic material"',
-                        },
-                        {
-                            thumbnail: "/assets/images/Artifacts.png",
-                            title: "Artifacts",
-                            link: "?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=COLLECTIONS&language=144&REPORT=WEB_UNION_SUM_COL&EXP=(M_IM_OPAC Yes)",
-                        },
-                        {
-                            thumbnail: "/assets/images/Textual Records.png",
-                            title: "Textual Records",
-                            link: '?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM&EXP=TEXT_READY Yes and FORM_CL "Textual record"',
-                        },
-                    ],
+              loop: true,
+              data: [
+                {
+                  thumbnail: "/assets/images/Photos.png",
+                  title: "Photographs",
+                  link: '?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM&EXP=(A_IM_OPAC Yes and FORM "Graphic material") or M_IM_OPAC Yes',
                 },
-            }, ],
-        },
-        {
-            component: "Section",
-            data: {
-                heading: "Browse by area",
+                {
+                  thumbnail: "/assets/images/Maps.png",
+                  title: "Maps",
+                  link: '?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM&EXP=(A_IM_OPAC Yes and FORM "Cartographic material") or M_IM_OPAC Yes',
+                },
+                {
+                  thumbnail: "/assets/images/Audio-Visual.png",
+                  title: "Audio/Video",
+                  link: '?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM&EXP=((A_AD_OPAC Yes or A_VD_OPAC Yes) and FORM "Cartographic material") or (M_AD_OPAC Yes or M_VD_OPAC Yes)',
+                },
+                {
+                  thumbnail: "/assets/images/Artifacts.png",
+                  title: "Artifacts",
+                  link: "?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=COLLECTIONS&language=144&REPORT=WEB_UNION_SUM_COL&EXP=(M_IM_OPAC Yes)",
+                },
+                {
+                  thumbnail: "/assets/images/Textual Records.png",
+                  title: "Textual Records",
+                  link: '?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&language=144&REPORT=WEB_UNION_SUM&EXP=(A_TX_OPAC Yes and FORM "Textual record") or (M_TX_OPAC Yes)',
+                },
+              ],
             },
-            children: [{
-                component: "Map",
-            }, ],
+          },
+        ],
+      },
+      {
+        component: "Section",
+        data: {
+          heading: "Browse by area",
         },
+        children: [
+          {
+            component: "Map",
+          },
+        ],
+      },
 
-        {
-            component: "Section",
-            data: {
-                heading: "Recent Additions",
-                description: "Recent additions from the Archives and Artifact Collections",
-            },
-            children: [{
-                component: "ImageMasonry",
-                data: {},
-            }, ],
+      {
+        component: "Section",
+        data: {
+          heading: "Recent Additions",
+          description:
+            "Recent additions from the Archives and Artifact Collections",
         },
+        children: [
+          {
+            component: "ImageMasonry",
+            data: {},
+          },
+        ],
+      },
     ],
-}, ];
+  },
+];
 export default Home;
