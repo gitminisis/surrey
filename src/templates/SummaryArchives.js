a41cc434812c69a61a9f165d744cb4bfb4235057
opacteam <mike@minisisinc.com>
2023-05-02 07:50:24 -0700

    update general search box

--- src/templates/SummaryArchives.js
+++ src/templates/SummaryArchives.js
@@ -1,45 +1,41 @@
 import {
-  DESCRIPTION_SUMMARY_FIELD,
-  GENERAL_SEARCHBOX_DBLIST,
-  MEDIA_THUMBNAIL_FIELD,
+    DESCRIPTION_SUMMARY_FIELD,
+    GENERAL_SEARCHBOX_DBLIST,
+    MEDIA_THUMBNAIL_FIELD,
 } from "./API";
 
-const Summmary = [
-  {
+const Summmary = [{
     component: "Layout",
     data: {
-      active: "Archives",
+        active: "Archives",
     },
-    children: [
-      {
+    children: [{
         component: "SummaryLayout",
         data: {
-          application: "DESCRIPTION",
-          generalSearchBox: {
-            breadcrumbs: ["Summary"],
-            heading: "Search the collections",
-            placeholder: "Search By Keyword",
-            databaseList: GENERAL_SEARCHBOX_DBLIST,
-            helpText: {
-              link: "/faq.html",
-              description:
-                "Didn't find what you're looking for? Check our FAQ page for search tips",
+            application: "DESCRIPTION",
+            generalSearchBox: {
+                breadcrumbs: ["Summary"],
+                heading: "Search the collections",
+                placeholder: "Search By Keyword",
+                databaseList: [{
+                    "database": "Archives",
+                    "searchURL": "?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=DESCRIPTION&language=144&REPORT=WEB_UNION_SUM_DESC"
+                }],
+                helpText: {
+                    link: "/faq.html",
+                    description: "Didn't find what you're looking for? Check our FAQ page for search tips",
+                },
             },
-          },
-          sortOptions: true,
-          filter: [],
-          thumbnailData: MEDIA_THUMBNAIL_FIELD,
-          displayField: [
-            {
-              database: "DESCRIPTION",
-              fields: DESCRIPTION_SUMMARY_FIELD,
-            },
-          ],
-          defaultView: "list",
+            sortOptions: true,
+            filter: [],
+            thumbnailData: MEDIA_THUMBNAIL_FIELD,
+            displayField: [{
+                database: "DESCRIPTION",
+                fields: DESCRIPTION_SUMMARY_FIELD,
+            }, ],
+            defaultView: "list",
         },
-      },
-    ],
-  },
-];
+    }, ],
+}, ];
 
 export default Summmary;
--- src/templates/SummaryArtifact.js
+++ src/templates/SummaryArtifact.js
@@ -1,64 +1,56 @@
 import {
-  COLLECTIONS_SUMMARY_FIELD,
-  MEDIA_THUMBNAIL_FIELD,
-  GENERAL_SEARCHBOX_DBLIST,
+    COLLECTIONS_SUMMARY_FIELD,
+    MEDIA_THUMBNAIL_FIELD,
+    GENERAL_SEARCHBOX_DBLIST,
 } from "./API";
 
-const Summary = [
-  {
+const Summary = [{
     component: "Layout",
     data: {
-      active: "Artifact",
+        active: "Artifact",
     },
-    children: [
-      {
+    children: [{
         component: "SummaryLayout",
         data: {
-          application: "COLLECTIONS",
-          generalSearchBox: {
-            breadcrumbs: ["Summary"],
-            heading: "Search the collections",placeholder:"Search By Keyword",
-            databaseList: GENERAL_SEARCHBOX_DBLIST,
-            helpText: {
-              link: "/faq.html",
-              description:
-                "Didn't find what you're looking for? Check our FAQ page for search tips",
+            application: "COLLECTIONS",
+            generalSearchBox: {
+                breadcrumbs: ["Summary"],
+                heading: "Search the collections",
+                placeholder: "Search By Keyword",
+                databaseList: [{
+                    "database": "Artifact",
+                    "searchURL": "?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]374.htm&APPLICATION=UNION_VIEW&DATABASE=COLLECTIONS&language=144&REPORT=WEB_UNION_SUM_COL"
+                }],
+                helpText: {
+                    link: "/faq.html",
+                    description: "Didn't find what you're looking for? Check our FAQ page for search tips",
+                },
             },
-          },
-          sortOptions: true,
+            sortOptions: true,
 
-          filter: [
-            {
-              item_group: [
-                {
-                  item_value: "Image",
-                  item_frequency: 11,
-                  item_link:
-                    "https://FORDHERITAGEVAULT.COM/SCRIPTS10/MWIMAIN.DLL/1015209066?SEARCH&amp;EXP=KEYWORD_CLUSTER~20~1Dtest~1D~20*~20(A_\nMEDIA_TYPE~20~22Image~22)",
-                  item_selected: "N",
-                },
-                {
-                  item_value: "Textual",
-                  item_frequency: 1054,
-                  item_link:
-                    "https://FORDHERITAGEVAULT.COM/SCRIPTS10/MWIMAIN.DLL/1015209066?SEARCH&amp;EXP=KEYWORD_CLUSTER~20~1Dtest~1D~20*~20(A_\nMEDIA_TYPE~20~22Textual~22)",
-                  item_selected: "N",
-                },
-              ],
-            },
-          ],
-          thumbnailData: MEDIA_THUMBNAIL_FIELD,
-          displayField: [
-            {
-              database: "COLLECTIONS",
-              fields: COLLECTIONS_SUMMARY_FIELD,
-            },
-          ],
-          defaultView: "grid",
+            filter: [{
+                item_group: [{
+                        item_value: "Image",
+                        item_frequency: 11,
+                        item_link: "https://FORDHERITAGEVAULT.COM/SCRIPTS10/MWIMAIN.DLL/1015209066?SEARCH&amp;EXP=KEYWORD_CLUSTER~20~1Dtest~1D~20*~20(A_\nMEDIA_TYPE~20~22Image~22)",
+                        item_selected: "N",
+                    },
+                    {
+                        item_value: "Textual",
+                        item_frequency: 1054,
+                        item_link: "https://FORDHERITAGEVAULT.COM/SCRIPTS10/MWIMAIN.DLL/1015209066?SEARCH&amp;EXP=KEYWORD_CLUSTER~20~1Dtest~1D~20*~20(A_\nMEDIA_TYPE~20~22Textual~22)",
+                        item_selected: "N",
+                    },
+                ],
+            }, ],
+            thumbnailData: MEDIA_THUMBNAIL_FIELD,
+            displayField: [{
+                database: "COLLECTIONS",
+                fields: COLLECTIONS_SUMMARY_FIELD,
+            }, ],
+            defaultView: "grid",
         },
-      },
-    ],
-  },
-];
+    }, ],
+}, ];
 
 export default Summary;
