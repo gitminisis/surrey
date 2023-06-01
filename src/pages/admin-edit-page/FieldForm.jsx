ec23740f83d11f61bb9a12822ec7fde86f13cc2c
opacteam <mike@minisisinc.com>
2023-06-01 12:30:15 -0700

    update dev

--- src/pages/admin-edit-page/FieldForm.jsx
+++ src/pages/admin-edit-page/FieldForm.jsx
@@ -3,6 +3,8 @@ import { InputLabel, Grid, Stack, OutlinedInput } from "@mui/material";
 const FieldForm = (props) => {
   const { data, handleChange } = props;
   let { name, description, type, value, defaultValue, editable } = data;
+  debugger;
+  
   if (!editable) {
     return null;
   }
