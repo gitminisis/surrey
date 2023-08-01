c484c44a437c957ea66253cf9585d4b8dcdebc55
opacteam <mike@minisisinc.com>
2023-08-01 06:56:20 -0700

    save changes on server

---
+++ dist_og/src_pages_extra-pages_SamplePage_js.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_pages_extra-pages_SamplePage_js"],{
+
+/***/ "./src/pages/extra-pages/SamplePage.js":
+/*!*********************************************!*\
+  !*** ./src/pages/extra-pages/SamplePage.js ***!
+  \*********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _mui_material__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @mui/material */ \"./node_modules/@mui/material/Typography/Typography.js\");\n/* harmony import */ var components_MainCard__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! components/MainCard */ \"./src/components/MainCard.js\");\n/* provided dependency */ var React = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n// material-ui\n\n\n// project import\n\n\n// ==============================|| SAMPLE PAGE ||============================== //\n\nvar SamplePage = function SamplePage() {\n  return /*#__PURE__*/React.createElement(components_MainCard__WEBPACK_IMPORTED_MODULE_0__[\"default\"], {\n    title: \"Sample Card\"\n  }, /*#__PURE__*/React.createElement(_mui_material__WEBPACK_IMPORTED_MODULE_1__[\"default\"], {\n    variant: \"body2\"\n  }, \"Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate descent molls anim id est labours.\"));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (SamplePage);\n\n//# sourceURL=webpack:///./src/pages/extra-pages/SamplePage.js?");
+
+/***/ })
+
+}]);
