fd504206948566683cdfa03eec9ca2e9e08b7f21
opacteam <mike@minisisinc.com>
2023-11-16 11:20:29 -0800

    move site-layout to its own folder, added site-layout schema

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -15,6 +15,7 @@ import MessageModal from "../MessageModal";
 const MAX_SNACK = 3;
 import announcement from "../../templates/json/announcement/data.json";
 const Layout = ({ active, children }) => {
+  console.log(SiteLayout);
   return (
     <SnackbarProvider
       maxSnack={MAX_SNACK}
